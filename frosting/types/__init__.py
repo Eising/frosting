e9659cacc0575d23d92723a7fddfd2f38d040d1e
Allan Eising <allan.eising@telia.no>
2018-11-26 16:20:53 +0100

    Provide a Frosting.types.TYPES variable
    Variable contains available types

--- frosting/types/__init__.py
+++ frosting/types/__init__.py
@@ -4,3 +4,8 @@ from frosting.types.textinput import TextInput
 from frosting.types.integerinput import IntegerInput
 
 __all__ = ['BaseType', 'TextInput', 'IntegerInput']
+
+TYPES = [
+    {'types.TextInput': 'String input, supports validator parameter'},
+    {'types.IntegerInput': 'Number input, supports limit parameter'}
+]
